b8c0ae287b93b794d229e1e41b3632bc00bfe004
Phil Ershadi <phildoge@gmail.com>
2018-03-02 01:01:02 -0500

    added list/board schema

--- src/models/task.js
+++ src/models/task.js
@@ -1,21 +1,22 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
-    title:{ 
-        type:String,
-        required: true,
-        index: {unique: true}
-    },
-    label: String,
-    comments: [String],  //TODO: add a comment schema?
-    updated:{
-        type: Date,
-        default: Date.now
-    },
-    members:[String],
-    dueDate: Date,
-    checklist: [String],  //TODO: add a checklist schema?
-    attachments: [String]  
-})
+  title: {
+    type: String,
+    required: true,
+    index: { unique: true }
+  },
+  label: String,
+  comments: [String], //TODO: add a comment schema?
+  updated: {
+    type: Date,
+    default: Date.now
+  },
+  members: [String],
+  dueDate: Date,
+  checklist: [String], //TODO: add a checklist schema?
+  attachments: [String]
+});
 
-module.exports = mongoose.model("task", taskSchema)
+var Task = mongoose.model("Task", taskSchema);
+module.exports = Task;
