8c9cc4f16893056cd1ab65b612251993866ce80c
Kenito Henry <kenitohenry@gmail.com>
2018-03-03 17:34:26 -0500

    Eddited the Navigation bar, looks more like Trello.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,15 @@ class Navbar extends Component{
     render(){
 return(
         <ul className="navbar-registered">
-            <li className="boards"> <a href="#"><i class="fab fa-trello"></i>Boards</a></li>
-            <li className="search"> <input type="search" name="search" placeholder="Buscar lol search..."/></li>
+            <li className="boards"><a href="#"><i class="fab fa-trello"></i> Boards</a></li>
+            <li className="search"> <input id="sb" type="text" name="search" placeholder="Buscar lol search..."/></li>
             <li className="logo"> <div><i class="fab fa-trello"></i>Trello</div></li>
+            <li class="button-group">
             <li className="plus"> <i class="fas fa-plus"></i></li>
             <li className="info"> <i class="fas fa-info-circle"></i></li>
             <li className="bell"> <i class="far fa-bell"></i></li>
             <li className="profile-pic"> <div ></div></li>
-                   
+               </li>    
                             
         </ul>
  ); }
