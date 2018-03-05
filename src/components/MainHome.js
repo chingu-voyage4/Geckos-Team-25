e0edc111063199bb0097aebe181277d2665f16cf
Kenito Henry <kenitohenry@gmail.com>
2018-03-05 17:18:26 -0500

    Main home component - for returning visitors after being signed in

--- src/components/MainHome.js
+++ src/components/MainHome.js
@@ -5,11 +5,11 @@ class MainHome extends Component{
     render(){
 return(
         <ul className="main-home">
-            <li className="text1">Welcome back to Trello! </li>
-            <li className="text2">Login to Trello</li>
-            <li className="text3"> <a href="#"><button><i class="fab fa-google"></i>Login with Google</button></a></li>
-            <li className="text"><a href="#"><i class="fas fa-email"></i>Login with Email and Password</a></li>  
-            <li className="text"><a href="#">Login with SSO</a></li>  
+            <li className="text1"><br/>Welcome back to Trello! </li>
+            <li className="text2"><br/>Login to Trello</li>
+            <li className="text3"> <br/><a href="#"><button><i class="fab fa-google"></i> Login with Google</button></a><br/></li>
+            <li className="text"><br/><a href="#"><i class="far fa-envelope"></i>Login with Email and Password</a></li>  
+            <li className="text"><br/><a href="#">Login with SSO</a></li>  
                             
         </ul>
  ); }
