9b7d1c4be8e50449aa050e1a58fbbd2e90a0cfd8
Kenito Henry <kenitohenry@gmail.com>
2018-03-04 21:50:48 -0500

    Main Home still needs a bit of styling

--- src/components/MainHome.js
+++ src/components/MainHome.js
@@ -4,12 +4,12 @@ import './MainHome.css';
 class MainHome extends Component{
     render(){
 return(
-        <ul className="navbar-registered">
+        <ul className="main-home">
             <li className="text1">Welcome back to Trello! </li>
             <li className="text2">Login to Trello</li>
-            <li className="text3"> <a href="#"><i class="fab fa-google"></i> Boards</a></li>
-            <li className="button-group"><a href="#"><i class="fab fa-trello"></i>Login with Email and Password</a>    </li>  
-            <li className=""><a href="#"><i class="fab fa-trello"></i>Login with SSO</a></li>  
+            <li className="text3"> <a href="#"><button><i class="fab fa-google"></i>Login with Google</button></a></li>
+            <li className="text"><a href="#"><i class="fas fa-email"></i>Login with Email and Password</a></li>  
+            <li className="text"><a href="#">Login with SSO</a></li>  
                             
         </ul>
  ); }
